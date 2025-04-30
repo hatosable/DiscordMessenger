96316481da34bd150b8c8e23ae43039ccf246099
IUG.Hattori <hato0621soda@gmail.com>
2025-04-30 16:11:09 +0900

    Add Supported Version
    5.5対応版をリリース

---
+++ DiscordMessenger_5.5/Intermediate/Build/Win64/UnrealGame/Inc/DiscordMessenger/UHT/DiscordMessageLibrary.generated.h
@@ -0,0 +1,63 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "DiscordMessageLibrary.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+struct FDiscordMessageContent;
+struct FDiscordMessageEmbeds;
+#ifdef DISCORDMESSENGER_DiscordMessageLibrary_generated_h
+#error "DiscordMessageLibrary.generated.h already included, missing '#pragma once' in DiscordMessageLibrary.h"
+#endif
+#define DISCORDMESSENGER_DiscordMessageLibrary_generated_h
+
+#define FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	DECLARE_FUNCTION(execSendMessageToDiscord);
+
+
+#define FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUDiscordMessageLibrary(); \
+	friend struct Z_Construct_UClass_UDiscordMessageLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UDiscordMessageLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/DiscordMessenger"), NO_API) \
+	DECLARE_SERIALIZER(UDiscordMessageLibrary)
+
+
+#define FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UDiscordMessageLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UDiscordMessageLibrary(UDiscordMessageLibrary&&); \
+	UDiscordMessageLibrary(const UDiscordMessageLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UDiscordMessageLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UDiscordMessageLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UDiscordMessageLibrary) \
+	NO_API virtual ~UDiscordMessageLibrary();
+
+
+#define FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h_12_PROLOG
+#define FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h_15_INCLASS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DISCORDMESSENGER_API UClass* StaticClass<class UDiscordMessageLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_HostProject_Plugins_DiscordMessenger_Source_DiscordMessenger_Public_DiscordMessageLibrary_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ DiscordMessenger_5.5/Intermediate/Build/Win64/UnrealGame/Inc/DiscordMessenger/UHT/DiscordMessenger.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeDiscordMessenger_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_DiscordMessenger;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_DiscordMessenger()
+	{
+		if (!Z_Registration_Info_UPackage__Script_DiscordMessenger.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/DiscordMessenger",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xB0EB4FA4,
+				0x4E4FD97D,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_DiscordMessenger.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_DiscordMessenger.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_DiscordMessenger(Z_Construct_UPackage__Script_DiscordMessenger, TEXT("/Script/DiscordMessenger"), Z_Registration_Info_UPackage__Script_DiscordMessenger, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB0EB4FA4, 0x4E4FD97D));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
